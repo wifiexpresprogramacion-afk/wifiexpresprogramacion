85516cdf18dc19eb9cf735727474ea204661ca3e
ddrsistemas <ddrsistemas@gmail.com>
2026-06-23 17:36:20 -0400

    subir

--- resources/views/livewire/mikrotik/aliado/list-advertising-campaign.blade.php
+++ resources/views/livewire/mikrotik/aliado/list-advertising-campaign.blade.php
@@ -182,7 +182,7 @@
                                     @endif
                                 @elseif($selected_id && $current_media_path)
                                     @if($media_type == 'imagen')
-                                        <img src="{{ \App\Models\AdvertisingCampaign::find($selected_id)->media_url }}" class="img-fluid rounded shadow-sm" style="max-height: 150px;">
+                                        <img src="{{ \App\Models\AdvertisingCampaign::find($selected_id)->media_url }}" class="img-fluid rounded shadow-sm" style="max-height: 200px;">
                                     @elseif($media_type == 'video')
                                         <video src="{{ \App\Models\AdvertisingCampaign::find($selected_id)->media_url }}" controls class="img-fluid rounded shadow-sm" style="max-height: 200px;"></video>
                                     @endif
