f3fe65c4ee3be448197e695fde8b10d8f1f52413
ddrsistemas <ddrsistemas@gmail.com>
2026-07-18 17:51:50 -0400

    subir

--- database/migrations/2026_07_18_170710_create_user_sucursals_table.php
+++ database/migrations/2026_07_18_170710_create_user_sucursals_table.php
@@ -15,6 +15,8 @@ class CreateUserSucursalsTable extends Migration
     {
         Schema::create('user_sucursals', function (Blueprint $table) {
             $table->id();
+            $table->bigInteger('user_id');
+            $table->bigInteger('router_id');
             $table->timestamps();
         });
     }
