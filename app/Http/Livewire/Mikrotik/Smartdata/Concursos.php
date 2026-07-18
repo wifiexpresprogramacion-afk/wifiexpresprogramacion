b2d2dcfc85ecaabafdac0851c2107a7c605baee2
ddrsistemas <ddrsistemas@gmail.com>
2026-07-18 09:12:09 -0400

    subir

--- app/Http/Livewire/Mikrotik/Smartdata/Concursos.php
+++ app/Http/Livewire/Mikrotik/Smartdata/Concursos.php
@@ -3,11 +3,39 @@
 namespace App\Http\Livewire\Mikrotik\Smartdata;
 
 use Livewire\Component;
+use Livewire\WithPagination;
+use App\Models\AdvertisingConcurso;
+use App\Models\AgeRange;
+use App\Models\Router;
+use Illuminate\Support\Facades\Auth;
 
 class Concursos extends Component
 {
+    use WithPagination;
+
+    public $search = '';
+
     public function render()
     {
-        return view('livewire.mikrotik.smartdata.concursos');
+        $query = AdvertisingConcurso::withCount('responses')
+            ->where('user_id', Auth::id());
+
+        if ($this->search) {
+            $query->where('name', 'like', '%' . $this->search . '%');
+        }
+
+        $ageRanges = AgeRange::where('user_id', Auth::id())->get();
+
+        $routers = Router::where('user_id', Auth::id())
+            ->whereHas('user', function($q) {
+                $q->whereIn('role', ['aliado', 'aliadoSmartData']);
+            })
+            ->get();
+
+        return view('livewire.mikrotik.smartdata.concursos', [
+            'concursos' => $query->latest()->paginate(10),
+            'ageRanges' => $ageRanges,
+            'routers' => $routers
+        ])->layout('layouts.app');
     }
 }
